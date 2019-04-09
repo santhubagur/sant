a3781409a8e351e28e61faeaf2a5303781b247ab
Cloud User <ec2-user@ip-172-31-4-108.ap-south-1.compute.internal>
2019-04-09 14:13:26 +0000

    removed the space

--- big3.c
+++ big3.c
@@ -1,10 +1,13 @@
 #include <stdio.h>
+
 void main()
 {
     int num1, num2, num3;
+   
     printf("Enter the values of num1, num2 and num3\n");
     scanf("%d %d %d", &num1, &num2, &num3);
     printf("num1 = %d\tnum2 = %d\tnum3 = %d\n", num1, num2, num3);
+   
     if (num1 > num2)
     {
         if (num1 > num3)
